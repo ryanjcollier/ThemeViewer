6a7966bdf6eff34f2337a17e5759692be09240b8
Ryan Collier <ryanjcollier@gmail.com>
2012-11-29 15:54:24 -0800

    Initial upload of application and theming files.

--- resources/sass/config.rb
+++ resources/sass/config.rb
@@ -8,6 +8,9 @@ load File.join(dir, '..', '..', 'touch', 'resources', 'themes')
 # Place compiled *.css files in the css directory 
 sass_path    = dir
 css_path     = File.join(dir, '..', 'css')
+#images_path  = File.join(dir, '..', 'images')
+http_images_path = "../images/"
+http_fonts_path = "../fonts/"
 output_style = :expanded
 environment  = :development
 sass_options = {:debuginfo => true}
